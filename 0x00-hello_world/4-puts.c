1a51e08ec1c9c10d60882568ac7bcc23ae304590
juan-skill <1294@holbertonschool.com>
2019-09-18 19:21:39 +0000

    update a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - Print a message. It should end with the value 0
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
