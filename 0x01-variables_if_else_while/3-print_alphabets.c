1edee2d7a828c95dd98c9aa2f7bd0a8219024793
juan-skill <1294@holbertonschool.com>
2019-09-19 21:10:18 +0000

    upgrade fixed c program

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int c;
 
-	for (c = 97; c <= 127; c++)
+	for (c = 97; c < 123; c++)
 	{
 		putchar(c);
 	}
-	for (c = 65; c <= 91; c++)
+	for (c = 65; c < 91; c++)
 	{
 		putchar(c);
 	}
