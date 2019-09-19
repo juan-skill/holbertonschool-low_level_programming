d3d953d389273f489103723a61ec066242ab3cc2
juan-skill <1294@holbertonschool.com>
2019-09-19 21:00:49 +0000

    upgrade c program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Display alphabet a-z then A-Z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 127; c++)
+	{
+		putchar(c);
+	}
+	for (c = 65; c <= 91; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
