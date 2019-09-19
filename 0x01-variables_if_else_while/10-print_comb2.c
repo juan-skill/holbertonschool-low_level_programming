5684d802c002192c411c4500cc019971f6690754
juan-skill <1294@holbertonschool.com>
2019-09-19 23:14:48 +0000

    update empty file

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - display all number 00 to 99
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int numx;
+	for (num = 48; num < 58; num++)
+	{
+		for (numx = 48; numx < 58; numx++)
+		{
+			putchar(num);
+			putchar(numx);
+			if (num == 57 && numx == 57)
+			{
+				putchar('\n');
+				break;
+			}
+			putchar(44);
+			putchar(32);
+		}
+	}
+	return (0);
+}
