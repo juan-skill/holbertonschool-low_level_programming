5888b700c1fce5711fe1272e566716ce6cd5aef0
juan-skill <1294@holbertonschool.com>
2019-09-19 01:47:55 +0000

    adding a C program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *main - write text to stdout (Standar Output)
+ *
+ *Return: return 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
