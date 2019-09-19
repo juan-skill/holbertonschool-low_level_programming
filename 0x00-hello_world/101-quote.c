a1db8d1b8f37a114b37519d0d3157fe931492206
juan-skill <1294@holbertonschool.com>
2019-09-19 02:15:28 +0000

    correction program in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\0", 59);
+	write(stdout, "and that piece of art is useful - Dora Korpar, 2015-10-19\0", 58);
 	return (1);
 }
