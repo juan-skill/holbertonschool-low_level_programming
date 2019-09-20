6a059e18089a337318e63d02597446511b7a8fa8
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-09-19 22:11:12 -0500

    fixed C program

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-	int n = '0';
-
-	while (n <= '9')
-	{
-		putchar(n++);
-	}
-	putchar('\n');
-	return (0);
+int n = '0';
+while (n <= '9')
+{
+	putchar(n++);
+}
+putchar('\n');
+return (0);
 }
