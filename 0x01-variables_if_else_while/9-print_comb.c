6ae5f454640c81a7f990a9d83d488a62fccf58ae
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-09-19 22:25:34 -0500

    update source code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (num == 57)
+{
+putchar('\n');
+break;
+}
+putchar(44);
+putchar(32);
+}
+return (0);
+}
