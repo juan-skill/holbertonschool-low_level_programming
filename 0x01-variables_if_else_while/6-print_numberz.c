b39cc16e09dc4db22b9cd73db9bc26eee46abe5f
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-09-19 22:06:57 -0500

    adding a c program

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - display all digits in base 10 numbers
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n++);
+	}
+	putchar('\n');
+	return (0);
+}
