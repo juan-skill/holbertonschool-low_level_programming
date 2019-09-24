213caadf998b3c1006f090ffe942bea7b5237c46
juan-skill <1294@holbertonschool.com>
2019-09-24 05:39:50 +0000

    udpate c program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns its result
+ *
+ * @n1: number1 to add
+ * @n2: number2 to add
+ * Return: sum of the two numbers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
