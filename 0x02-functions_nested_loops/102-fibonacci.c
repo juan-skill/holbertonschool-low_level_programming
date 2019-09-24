26143c6cbd3d23b0ff69a83a1fb3fc3aaf5fe821
juan-skill <1294@holbertonschool.com>
2019-09-24 21:36:20 +0000

    update fixed c program

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,12 +11,11 @@ int main(void)
 	int i, n = 50;
 	long x = 0, y = 1;
 
-	printf("0 1 ");
 	for (i = 2; i < n; i++)
 	{
 		y = y + x;
 		x = y - x;
-		printf("%lu ", y);
+		printf("%ld ", y);
 	}
 
 	return (0);
