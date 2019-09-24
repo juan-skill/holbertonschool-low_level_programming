d5e203d0a84c9736d63e99da3673d8fc279ef85c
juan-skill <1294@holbertonschool.com>
2019-09-24 05:18:35 +0000

    update c program

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - print Holberton using _putchar function
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
