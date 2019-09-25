267d85965e1ec00030fd0191de31b1e3d2adcad6
juan-skill <1294@holbertonschool.com>
2019-09-25 03:30:39 +0000

    update fixed c program

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,15 +4,14 @@
  * main - prints the first 50 Fibonacci numbers
  *
  * Return: 0 Always (Success)
- *
  */
 int main(void)
 {
 	long i, num1, num2, num3;
 
-	num3 = 3;
 	num1 = 1;
 	num2 = 2;
+	num3 = 3;
 
 	printf("1, 2, ");
 	for (i = 0; i < 47; i++)
