4be756b628cba8d85a4ed7e7021312c0a20dbba8
juan-skill <1294@holbertonschool.com>
2019-09-26 22:20:37 +0000

    update c program

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		_putchar('0' + num);
+		num++;
+	}
+	_putchar('\n');
+}
