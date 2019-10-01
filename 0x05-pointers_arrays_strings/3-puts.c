7a721fd8e4d31b244ad467d84adc868484cfeed1
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-10-01 04:54:19 -0500

    update c program file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int len;
+
+  len = 0;
+	while (*s != '\0')
+		_putchar(s++);
+	_putchar(10);
+}
