a7107adf50aef974ebf64a23ab5d1babdfc2450e
juan-skill <1294@holbertonschool.com>
2019-10-01 17:25:50 +0000

    update fixed program

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 
 /**
+ * _strlen - returns the length of a string
+ *
+ * @s: string to check the length
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
  * puts2 - prints every other character of a string,
  * starting with the first character
  *
@@ -10,12 +27,14 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; str[i] != '\0'; i++)
+	len = _strlen(str);
+	i = 0;
+	while (i < len)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
