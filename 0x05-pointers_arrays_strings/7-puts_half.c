3dfac37f61caf5f8801803794d159aaa0f4ea922
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-10-01 05:28:43 -0500

    update c program file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string *
+ *
+ * @s: string to check the length 
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string to print half
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	i = _strlen(str);
+	if (i % 2 != 0)
+		i++;
+  
+	i /= 2;
+	while (str[i] != '\0')
+		_putchar(str[i++]);
+	_putchar(10);
+}
