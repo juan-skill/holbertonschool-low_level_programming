d6c9a0d367948d0cb31929c601c842e788bb39aa
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-10-01 05:18:28 -0500

    update c program file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string 
+ *
+ * @s: string to check the length
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
+ * rev_string - reverses string in place, 
+ * without printing it
+ *
+ * @s: string to reverse
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	j = _strlen(s) - 1;
+	i = 0;
+	while (i < j)
+	{
+		temp1_c = s[j];
+		s[j] = s[i++];
+		s[i++] = temp;		
+	}
+}
