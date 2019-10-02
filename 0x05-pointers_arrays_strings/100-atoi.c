ee23a6043ecb05f47835a01d4b41357508cd9354
juan-skill <1294@holbertonschool.com>
2019-10-02 05:21:01 +0000

    update fixed c program

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,13 +16,13 @@ int _atoi(char *s)
 
 	num = 0;
 	neg = -1;
-	for (i = 0; s[i] < '0' || s[i] > '9') && s[i] != '\0'; i++)
+	for ((i = 0; s[i] < '0' || s[i] > '9') && s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
 			neg *= -1;
 	}
-  
-	while (s[i] < '0' || s[i] > '9') && s[i] != '\0')
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 		num = (num * 10) - (s[i++] - '0');
 	return (num * neg);
 }
