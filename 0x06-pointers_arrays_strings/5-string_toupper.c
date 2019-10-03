b24e3ea94acf7d9310be1faa5c19ac7805491c93
juan-skill <1294@holbertonschool.com>
2019-10-03 22:15:48 +0000

    update c program file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @str: string to changes from lowercase to upppers
+ * Return: a pointer to char that points an lowercase string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] > 'a' && str[i] < 'z')
+			str[i] -= ' ';
+		i++;
+	}
+	return (str);
+}
