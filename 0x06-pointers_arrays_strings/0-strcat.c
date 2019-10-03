5521c71ffd42815eb25df8fef303057d8e37a29d
juan-skill <1294@holbertonschool.com>
2019-10-03 17:48:41 +0000

    update fixed c program file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = 0;
-	while (dest[i] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
 		i++;
-	j = 0;
-	while (dest[j] != '\0')
-		dest[i++] = src[j++];
+	}
 	dest[i] = '\0';
 
 	return (dest);
