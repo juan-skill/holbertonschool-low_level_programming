fc9c7e7e18d4a95c7504d7a75b54ae90fcbd7f74
juan-skill <1294@holbertonschool.com>
2019-10-04 04:38:40 +0000

    update c program file

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rot13 - converts upper/lowercase  to rot13
+ * *
+ * @s: string to convert to rot13
+ * Return: returns converted string
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+		       (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'N' && s[i] <= 'Z') ||
+			    (s[i] >= 'n' && s[i] <= 'z'))
+				s[i] -= 13;
+			else
+				s[i] += 13;
+			i++;
+		}
+	}
+	return (s);
+}
