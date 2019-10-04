8e6229b652f5fa362f0a45c90ececc779e6d40ff
juan-skill <1294@holbertonschool.com>
2019-10-04 04:47:17 +0000

    update c program file

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,16 +9,15 @@
 char *rot13(char *s)
 {
 	int i;
+	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char storel[] = "nopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
-			if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-				s[i] -= 13;
-			else
-				s[i] += 13;
-			i++;
+			s[i] = (s[i] - 65 > 25) ?
+				storel[s[i] - 97] : storeh[s[i] - 65];
 		}
 	}
 	return (s);
