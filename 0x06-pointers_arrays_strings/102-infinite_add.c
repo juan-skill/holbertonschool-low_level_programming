1ee26f5f4101da570e29df49becf2d51999b3a29
juan-skill <1294@holbertonschool.com>
2019-10-05 03:23:58 +0000

    update version1

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - function that adds two numbers
+ *
+ * @n1: int type date
+ * @n2: int type date
+ * @r: is the buffer that the function will use store the result
+ * @size_r: is the buffer size
+ * Return: pointer to char that contains to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	
+}
