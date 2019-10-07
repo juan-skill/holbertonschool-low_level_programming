f67b63b03ea13bbfbcdc989419aee3b0da3c6a6b
juan-skill <1294@holbertonschool.com>
2019-10-07 18:51:42 +0000

    update c program file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
+	return (s + 1);
+}
