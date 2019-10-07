9dec87326ffc4ab28515a98d1d1b18e87d579493
juan-skill <1294@holbertonschool.com>
2019-10-07 02:47:25 +0000

    update empty file

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_buffer - prints a buffer
+ * (hexadecimal format)
+ *
+ * @b: buffer to print
+ * @size: size of the buffer
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+
+}
