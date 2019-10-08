26c181fa2763ed21d27c1605403a39b527771363
juan-skill <1294@holbertonschool.com>
2019-10-08 09:42:17 +0000

    update c program file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: chessboard value
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i != 7 || j != 7)
+	{
+		_putchar(a[i][j]);
+		if (j == 7)
+		{
+			_putchar('\n');
+			j = 0;
+			i++;
+		}
+		else
+			j++;
+	}
+	_putchar(a[i][j]);
+	_putchar(10);
+}
