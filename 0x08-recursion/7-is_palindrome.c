b52fa547b63ba68d8d618f237aa45cde2669b8d3
juan-skill <1294@holbertonschool.com>
2019-10-09 23:22:17 +0000

    Add solution for 0x08-7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string
+ *
+ * @s: a string to computes
+ * Return: the lenght of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
+/**
+ * _is_palindrome - check whether string is palindrome
+ *
+ * @s: string
+ * Return: 1 if it is palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int start = 0;
+	int end = (_strlen_recursion(s) - 1);
+
+	if (s[start] == s[end])
+		return (1);
+	else
+		is_palindrome(++s);
+	return (0);
+}
