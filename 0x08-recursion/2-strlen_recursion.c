15e7aa1b4928b25b7f51d6eac0b77124db6578f8
juan-skill <1294@holbertonschool.com>
2019-10-09 17:45:58 +0000

    Add solution for 0x08-2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string
+ *
+ * @s: a string to computes
+ * Return: the lenght of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
