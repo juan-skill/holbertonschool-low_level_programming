0006edc5ff6ff11815da1e3ab624ff1f77d64ff9
juan-skill <1294@holbertonschool.com>
2019-10-12 19:57:24 +0000

    Add solution for 0x0A-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name by which the
+ * program was called
+ *
+ * @argc: size of argv
+ * @argv: command vector arguments
+ * Return: 0 Always (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
