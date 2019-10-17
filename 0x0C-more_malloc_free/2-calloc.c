e732415f5cc1ffa31a81ce1b6b50659ffbc0fce4
juan-skill <1294@holbertonschool.com>
2019-10-17 19:32:11 +0000

    Edit solution for 0x0C-2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -32,15 +32,17 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int s;
 
 	if (nmemb == 0 && size == 0)
 		return (NULL);
-
-	ptr = (void *)malloc(nmemb * size);
+	
+	s = nmemb * size;
+	ptr = (void *)malloc(s);
 	if (ptr == NULL)
 		return (NULL);
 
-	_memset(ptr, 0, nmemb * size);
+	_memset(ptr, 0, s);
 
 	return (ptr);
 }
