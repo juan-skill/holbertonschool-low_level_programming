7fadf3c1a05ebd4cf9c93534200225222bf650e4
juan-skill <1294@holbertonschool.com>
2019-10-20 23:25:26 +0000

    Update header file includes 101

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,5 +6,9 @@ char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
 void *_realloc(void *, unsigned int, unsigned int);
+void error(int status);
+int *multiply(int *mul, char *, char *, int, int);
+void print_number(int *,int, int);
+int _putchar(char);
 
 #endif
