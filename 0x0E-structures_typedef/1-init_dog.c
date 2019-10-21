eb558cbddc00ba066d85ee54b5797f142b520396
juan-skill <1294@holbertonschool.com>
2019-10-21 19:22:23 +0000

    Add solution for 0x0E-1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: address of a pointer of type struct dog
+ * @name: a pointer that points to string name
+ * @age: age of the dog
+ * @owner: a pointer that points to string owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	*(d).name = name;
+	*(d).age = age;
+	*(d).owner = owner;
+}
