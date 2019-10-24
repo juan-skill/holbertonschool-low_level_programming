4a6b30ad12209a3609dbe71911fd2cfdf303f2f1
juan-skill <1294@holbertonschool.com>
2019-10-24 13:13:37 +0000

    Edit solution, for 0x0F-3main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,17 +20,16 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	o = get_op_func(argv[2]);
 	if (o == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
