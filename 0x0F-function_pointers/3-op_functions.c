605fe1a91502c46211f7878f89b511993a555226
juan-skill <1294@holbertonschool.com>
2019-10-24 03:36:29 +0000

    Add solution for 0x0F-3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: number one
+ * @b: number two
+ * Return: sum of the two
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ * @a: number one
+ * @b: number two
+ * Return: rest of the two
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiply two numbers
+ * @a: number one
+ * @b: number two
+ * Return: multiply of the two
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two numbers
+ * @a: number one
+ * @b: number two
+ * Return: divide of the two
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - module two numbers
+ * @a: number one
+ * @b: number two
+ * Return: modules of the two
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
