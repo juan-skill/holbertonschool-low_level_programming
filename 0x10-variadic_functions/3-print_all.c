106deb4d047c079bd8afeee756e949a93321230f
juan-skill <1294@holbertonschool.com>
2019-10-26 18:50:38 +0000

    Edit solution for 0x10-3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,10 +1,8 @@
 #include "variadic_functions.h"
 
 /**
- *
- *
- *
- *
+ * _print_char - print char type element from va_list
+ * @valist: va_list passed to function
  */
 void _print_char(va_list valist)
 {
@@ -12,11 +10,8 @@ void _print_char(va_list valist)
 }
 
 /**
- *
- *
- *
- *
- *
+ * _print_int - print int type element from va_list
+ * @valist: va_list passed to function
  */
 void _print_int(va_list valist)
 {
@@ -24,43 +19,50 @@ void _print_int(va_list valist)
 }
 
 /**
- *
- *
- *
- *
- *
+ * _print_string- print string element from va_list
+ * @valist: va_list passed to function
  */
 void _print_string(va_list valist)
 {
 	char *s;
+
 	s = va_arg(valist, char *);
+
 	if (s != NULL)
 		printf("%s", s);
 	else
 		printf("(nil)");
 }
 
-void _print_float(va_list valist )
+/**
+ * _print_float - print float type element from va_list
+ * @valist: va_list passed to function
+ */
+void _print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+
 
 /**
- * print_all - prints 
- *
- *
- *
+ * print_all - print all passsed if char, int, float, or string
+ * @format: string of formats to use and print
  */
 void print_all(const char * const format, ...)
 {
 
 	forma_t _formates[] = {
 		{'c', _print_char},
+		{'f', _print_float},
 		{'i', _print_int},
 		{'s', _print_string},
 		{'\0', NULL}
 	};
-
-	va_list valist;
 	int i, j;
+	char *separator;
+	va_list valist;
 
+	separator = "";
 	va_start(valist, format);
 
 	i = 0;
@@ -71,7 +73,9 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == _formates[j].c)
 			{
+				printf("%s", separator);
 				_formates[j].p_f(valist);
+				separator = ", ";
 			}
 			j++;
 		}
