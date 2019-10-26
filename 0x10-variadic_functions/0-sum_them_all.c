338efd208f9354433bbe7b67f0cd3bd095120b0a
juan-skill <1294@holbertonschool.com>
2019-10-26 18:47:53 +0000

    Edit solution for 0x10-0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,9 @@
 #include "variadic_functions.h"
 
-
 /**
  * sum_them_all - returns the sum of all its parameters
  *
  * @n: number of elements
- *
  * Return: the sum of all them, If n == 0, return 0
  */
 int sum_them_all(const unsigned int n, ...)
