83d912550c1414892400741b5f444ecb8bfac8a0
juan-skill <55102805+juan-skill@users.noreply.github.com>
2019-10-31 23:44:02 -0500

    [Add] adding file for 0x12-101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+	extern printf
+	global main
+	section .text
+  
+main:
+	push rbp
+	mov rdi, fmt 		;arg3, length of string
+	mov rsi, msg 		;arg2, pointer to string
+	mov rax, 0 		  ;arg1, write to screen
+	call printf     ;call printf function
+	pop rbp
+	mov rax,0
+	ret
+  
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
