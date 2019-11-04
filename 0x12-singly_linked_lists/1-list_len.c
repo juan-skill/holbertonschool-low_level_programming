3ba4643ee59f8c2bf680c9b8e1d588abf51778ff
juan-skill <1294@holbertonschool.com>
2019-11-04 07:00:32 +0000

    [Edit] validity parameters and create a pointer for traversing

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,14 +7,19 @@
  */
 size_t list_len(const list_t *head)
 {
-	size_t i;
+	size_t i = 0;
+	const list_t *tmp = NULL;
 
+	/*if (!head)*/
 	if (head == NULL)
 		return (0);
 
-	for (i = 0; head != NULL; i++)
+	tmp = head;
+	/*while (head)*/
+	while (tmp != NULL)
 	{
-		head = head->next;
+		tmp = tmp->next;
+		i++;
 	}
 	return (i);
 }
