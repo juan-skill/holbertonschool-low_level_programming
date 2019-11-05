62e5ec3d8eb0438ef151596ae73d3363fb0d68ad
juan-skill <1294@holbertonschool.com>
2019-11-05 09:42:33 +0000

    [Edit] includes data structure listint_s && print_listint prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,8 +4,23 @@
 /*libraries*/
 #include <stdio.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 /*prototypes*/
 int _putchar(char);
+size_t print_listint(const listint_t *);
 
 
 #endif
