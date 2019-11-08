6f921cf15acfba27002f1718829be099fcec1d55
juan-skill <1294@holbertonschool.com>
2019-11-08 01:13:48 +0000

    [Edit] includes binary_to_uinte() prototype function 	modified:   holberton.h

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -7,5 +7,7 @@
 /*prototype functions*/
 int _putchar(char);
 unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
