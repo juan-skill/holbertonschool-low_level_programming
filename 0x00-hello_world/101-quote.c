b6de4a56c26409c5f92900fda1e5a5dc5c91680d
juan-skill <1294@holbertonschool.com>
2019-11-10 03:07:30 +0000

    [Fix] Add the expression \" str variable 	modified:   101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int main(void)
 	char *str = NULL;
 	int length = 0;
 
-	str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	length = strlen(str);
 
 	write(2, str, length);
