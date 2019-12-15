a3ebeaeb3e947113e3d4990b43b1be63e01afb70
juan-skill <1294@holbertonschool.com>
2019-12-15 05:26:57 +0000

    [Fix] Uninitialised value was created by a heap allocation 	modified:   7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,7 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	n_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (n_node == NULL)
 		return (NULL);
-	n_node->n = n;
+	n_node->n = n, n_node->next = NULL, n_node->prev = NULL;
 	if (idx == 0)
 	{
 		n_node->prev = NULL;
