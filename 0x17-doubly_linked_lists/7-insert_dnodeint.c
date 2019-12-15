cf4ed78705664432cba4e4b07bbaa4887774200b
juan-skill <1294@holbertonschool.com>
2019-12-15 03:23:04 +0000

    [Fix] changes while options 	modified:   7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,20 +44,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	length = dlistint_len(*h);
 	if (idx > length)
 		return (NULL);
+
 	n_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (n_node == NULL)
 		return (NULL);
-	n_node->n = n, n_node->prev = NULL, n_node->next = NULL;
 
-	count = 0;
-	tmp = *h;
+	n_node->n = n;
+
 	if (idx != 0)
 		idx -= 1;
-	while (tmp != NULL && count < idx)
-	{
+
+	tmp = *h;
+	count = 0;
+	while (count++ < idx)
 		tmp = tmp->next;
-		count++;
-	}
+
 	if (idx == 0)
 	{
 		n_node->prev = NULL;
