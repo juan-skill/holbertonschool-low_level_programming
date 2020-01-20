8da7734d1603ff1d1394413ff593c8461307de43
juan-skill <1294@holbertonschool.com>
2020-01-20 01:29:01 +0000

    [Add] adding solution for 0x1A-2 	new file:   2-key_index.c 	modified:   hash_tables.h

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+
+/**
+ * key_index - get back the index of a key
+ *
+ * @key: is the key
+ * @size: the size of the array of the hash table
+ * Return: index at which the key/value pair should be stored in the
+ * array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2((unsigned char *) key) % size);
+}
