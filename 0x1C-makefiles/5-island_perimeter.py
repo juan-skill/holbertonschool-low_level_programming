910caacf5e18cc04d07840cffb3841027e14ffe7
juan-skill <1294@holbertonschool.com>
2020-02-22 04:59:48 +0000

    [Fix] incorrect variable 	modified:   5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,19 +16,19 @@ def island_perimeter(grid):
                     if grid[i - 1][j] == 0 or i == 0:
                         perim += 1
                 except IndexError:
-                    perimeter += 1
+                    perim += 1
 
                 try:
                     if grid[i + 1][j] == 0:
                         perim += 1
                 except IndexError:
-                    perimeter += 1
+                    perim += 1
 
                 try:
                     if grid[i][j - 1] == 0 or j == 0:
                         perim += 1
                 except IndexError:
-                    perimeter += 1
+                    perim += 1
 
                 try:
                     if grid[i][j + 1] == 0:
