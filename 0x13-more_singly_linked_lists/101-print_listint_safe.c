d29802d2034b125425949f7af497fb05cf0d8211
juan-skill <1294@holbertonschool.com>
2020-03-22 16:29:36 +0000

    [Fix] betty conding style 	modified:   101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,46 +8,22 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	/* const listint_t *tmp2, *tmp; */
 	size_t i = 0;
 
 	if (head == NULL)
 		return (0);
 
-<<<<<<< HEAD
-	tmp = head;
-	flag = i = 0;      /* count of nodes */
-
-	while (tmp != NULL)
-=======
 	while (head != NULL)
->>>>>>> featurejuan
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		i++;
 
 		if (head < head->next)
 		{
-<<<<<<< HEAD
-			if (tmp2 == tmp)
-			{
-				printf("-> [%p] %d\n", (void *)tmp2, tmp2->n);
-				flag = 1;
-				break;
-			}
-			tmp2 = tmp2->next;
-		}
-		if (flag == 1)
-=======
-			/* printf(" head < head->next [%p] %d > [%p] %d\n", (void *)head, head->n, (void *)head->next, head->next->n); */
-			/* printf(" head %ld > head->next %ld       head = head->next\n", i, i + 1); */
 			head = head->next;
 			printf("-> [%p] %d\n", (void *)head, head->n);
->>>>>>> featurejuan
 			break;
 		}
-		/* 	printf(" head > head->next [%p] %d > [%p] %d\n", (void *)head, head->n, (void *)head->next, head->next->n); */
-		/* 	printf(" head %ld > head->next %ld       head = head->next\n", i, i + 1); */
 		head = head->next;
 	}
 
